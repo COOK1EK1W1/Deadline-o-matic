3497d20f5edb579ae347ea3484cd7279e4537ea0
Ciaran Cook <ciarancook1@gmail.com>
2022-12-29 04:19:32 +0000

    added remove feature fixed add

--- src/deadline_cog.py
+++ src/deadline_cog.py
@@ -13,25 +13,21 @@ class DeadlineCog(commands.Cog, name='Deadlines'):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("Deadline Cog Loaded")
+    @commands.command()
+    async def sync(self, ctx) -> None:
+        await ctx.send(f'syncing')
+        fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+
+        await ctx.send(f'Synced {len(fmt)} commands.')
 
     @app_commands.command(name="all", description="display all the deadlines")
     async def all(self, interaction: discord.Interaction) -> None:
         """displays all the deadlines"""
-        deadlines = q_deadlines("SELECT * FROM deadlines WHERE due >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)")
+        deadlines = q_deadlines("SELECT * FROM deadlines")
         if len(deadlines) == 0:
             await interaction.response.send_message("no deadlines :)")
             return
         await interaction.response.send_message(embed=dl.format_deadlines_for_embed(deadlines, "All Deadlines"))
-
-    @commands.command()
-    async def sync(self, ctx) -> None:
-        print("doing stuff")
-        fmt = await ctx.bot.tree.sync(guild=ctx.guild)
-
-        await ctx.send(f'Synced {len(fmt)} commands.')
 
     @app_commands.command()
     async def past(self, interaction: discord.Interaction):
@@ -89,82 +85,22 @@ class DeadlineCog(commands.Cog, name='Deadlines'):
                 await interation.response.send_message(embed=deadlines[0].format_for_embed())
         else:
             deadlines = q_deadlines("SELECT * FROM deadlines")
-            best_match = dl.get_best_match(deadlines, " ".join(searchterm))
+            best_match = dl.get_best_match(deadlines, searchterm)
             if best_match is None:
                 await interation.response.send_message("no deadlines :)")
                 return
             await interation.response.send_message(embed=best_match.format_for_embed())
 
-    @commands.command()
-    async def add(self, ctx, *, args=None):
-        args = args.split("-")
-        if len(args) < 2:
-            return
-
-        name = ""
-        course = ""
-        start = None
-        due = None
-        mark = 0
-        room = ""
-        url = ""
-        info = ""
-        for i in range(0, len(args)):
-            if args[i][:2] == "n=":
-                name = args[i][2:]
-            elif args[i][:2] == "c=":
-                course = args[i][2:]
-            elif args[i][:2] == "s=":
-                start = args[i][2:]
-            elif args[i][:2] == "d=":
-                due = args[i][2:]
-            elif args[i][:2] == "m=":
-                mark = args[i][2:]
-            elif args[i][:2] == "r=":
-                room = args[i][2:]
-            elif args[i][:2] == "u=":
-                url = args[i][2:]
-            elif args[i][:2] == "i=":
-                info = args[i][2:]
-
+    @app_commands.command()
+    async def add(self, interaction: discord.Interaction, name: str, course: str, start: str=None, due: str=None, mark: float=0.0, room: str="", url: str="", info: str=""):
         query("""INSERT INTO deadlines
 (name, subject, `start`, due, mark, room, url, info)
 VALUES(%s, %s, %s, %s, %s, %s, %s, %s);""", (name, course, start, due, mark, room, url, info))
-        print(args)
+        await interaction.response.send_message("deadline added")
 
-    @commands.command()
-    async def add(self, ctx, *, args=None):
-        args = args.split("-")
-        if len(args) < 2:
-            return
-
-        name = ""
-        course = ""
-        start = None
-        due = None
-        mark = 0
-        room = ""
-        url = ""
-        info = ""
-        for i in range(0, len(args)):
-            if args[i][:2] == "n=":
-                name = args[i][2:]
-            elif args[i][:2] == "c=":
-                course = args[i][2:]
-            elif args[i][:2] == "s=":
-                start = args[i][2:]
-            elif args[i][:2] == "d=":
-                due = args[i][2:]
-            elif args[i][:2] == "m=":
-                mark = args[i][2:]
-            elif args[i][:2] == "r=":
-                room = args[i][2:]
-            elif args[i][:2] == "u=":
-                url = args[i][2:]
-            elif args[i][:2] == "i=":
-                info = args[i][2:]
-
-        query("""INSERT INTO deadlines
-(name, subject, `start`, due, mark, room, url, info)
-VALUES(%s, %s, %s, %s, %s, %s, %s, %s);""", (name, course, start, due, mark, room, url, info))
-        print(args)
+    @app_commands.command()
+    async def remove(self, intertaction: discord.Interaction, name: str):
+        deadlines = q_deadlines("SELECT * FROM deadlines")
+        best_match = dl.get_best_match(deadlines, name)
+        query("DELETE FROM deadlines WHERE name=%s AND subject=%s", (best_match.name, best_match.subject))
+        await intertaction.response.send_message("removed")
