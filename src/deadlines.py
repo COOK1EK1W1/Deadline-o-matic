f6569285b4e1cdff4d730886d1de683ebf5e3112
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2023-01-16 09:24:08 +0000

    updated courses

--- src/deadlines.py
+++ src/deadlines.py
@@ -138,7 +138,7 @@ def format_deadlines_for_embed(deadlines: list[Deadline], heading: str = "") -> 
         if deadline.due_in_past():
             strike = "~~"
 
-        colours = {"F28ED": ":test_tube:", "F28PL": ":keyboard:", "F28SG": ":classical_building:", "F28WP": ":globe_with_meridians:"}
+        colours = {"F28HS": "computer", "F28DM": "", "F28DA": ":classical_building:", "F28SD": ":keyboard:"}
         embed.add_field(name=f"{strike}{colours.get(deadline.subject)} {deadline.name} | {deadline.subject}{strike}", value=date_string + "\n ​", inline=False)  # beware the 0 width space thing used to make empty lines
     return embed
 
