2af71ce51813f9951fe93edc9478390d97a1d118
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2023-03-03 10:28:33 +0000

    changed annoucements

--- src/announcements.py
+++ src/announcements.py
@@ -6,7 +6,7 @@ from sql_interface import q_deadlines
 
 async def send_announcement_start(deadline: dl.Deadline, channel, for_time: datetime.datetime):
     embed = deadline.format_for_embed()
-    await channel.send(deadline.name + " starts " + dl.dt(deadline.due_datetime, "t"), embed=embed)
+    await channel.send(deadline.name + " starts " + dl.dt(deadline.start_datetime, "t"), embed=embed)
 
     
 async def send_announcement_due(deadline: dl.Deadline, channel, for_time: datetime.datetime):
